a42f54f306cc8bb5bbe15cbb5d9aad416c6debf8
Abhinav Jain <jabhi678@gmail.com>
2019-02-20 22:20:07 +0530

    ui update

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  currentUrl: string;
+
+  constructor(private router: Router) {
+    router.events.subscribe(_ => {
+      if (_ instanceof NavigationEnd) {
+        this.currentUrl = _.url;
+      }
+    });
+  }
 
   ngOnInit() {
   }
