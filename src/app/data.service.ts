1d4770ce99e5684d4dc5f6a1f88013690fcafa08
Abhinav Jain <jabhi678@gmail.com>
2019-02-20 20:18:07 +0530

    user comp

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,4 +12,12 @@ export class DataService {
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users')
   }
+
+  getUser(userId) {
+    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+  }
+
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+  }
 }
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  user$: Object;
+
+  constructor(private data: DataService, private route: ActivatedRoute) { 
+    this.route.params.subscribe(
+      params => this.user$ = params.id
+    )
+  }
 
   ngOnInit() {
+    this.data.getUser(this.user$).subscribe(
+      data => this.user$ = data
+    )
   }
 
 }
