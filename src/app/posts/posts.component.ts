7ab47e210b8f85d4a4061ecbde035e0b1b06af62
Abhinav Jain <jabhi678@gmail.com>
2019-02-20 20:41:57 +0530

    posts setup

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts$: Object;
+  
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getPosts().subscribe(
+      data => this.posts$ = data
+    )
   }
 
 }
